416f96a88c3cd5e6ef8a715ccfeebb5155b96d97
Pouee <2919877504@qq.com>
2018-10-27 23:45:32 +0800

    add some new files

--- src/net/TestServer.java
+++ src/net/TestServer.java
@@ -5,15 +5,16 @@ import java.net.Socket;
 
 public class TestServer {
 	public static void main(String[] args) throws Exception{
-		ServerSocket ss = new ServerSocket(10000);
+		ServerSocket ss = new ServerSocket(80);
 		Socket s1 = ss.accept();
-		byte []buf =new byte[1024];
-		int len = s1.getInputStream().read(buf);
-		System.out.println(new String(buf,0,len));
+		System.out.println("recv ");
+//		byte []buf =new byte[1024];
+//		int len = s1.getInputStream().read(buf);
+//		System.out.println(new String(buf,0,len));
 		
-		Socket s2 = ss.accept();
-		len = s2.getInputStream().read(buf);
-		System.out.println(new String(buf,0,len));
+//		Socket s2 = ss.accept();
+//		len = s2.getInputStream().read(buf);
+//		System.out.println(new String(buf,0,len));
 		
 		
 		Thread.sleep(100000);
